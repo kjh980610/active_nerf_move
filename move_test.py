5c6be2bd0762e1f05d476abb4bb94b3039498d09
kjh980610 <129719906+kjh980610@users.noreply.github.com>
2023-08-09 13:30:33 +0900

    Update move_test.py

--- move_test.py
+++ move_test.py
@@ -9,12 +9,16 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.msg import FollowJointTrajectoryAction, \
                              FollowJointTrajectoryGoal, FollowJointTrajectoryResult
 
-def go_joint_paths(joint_paths):
-    for joint_pose in joint_paths:
-        go_to_joint_state(joint_pose)
+def go_joint_paths(joint_path):
+    l = len(joint_path)
+    end_path=False
+    for i, joint_pose in enumerate(joint_path):
+        if i is l-1:
+            end_path = True
+        go_to_joint_state(joint_pose, end_path)
 
-def go_to_joint_state(joint_pose):
-    ros.init_node('move_to_start')
+def go_to_joint_state(joint_pose,end_path):
+    ros.init_node('move_joint')
 
     action = ros.resolve_name('effort_joint_trajectory_controller/follow_joint_trajectory')
     client = SimpleActionClient(action, FollowJointTrajectoryAction)
@@ -33,10 +37,9 @@ def go_to_joint_state(joint_pose):
     # poses = [0, 0.1963495375, 0.00, -2.616, 0.00, 2.9415926, 0.79]
 
     if joint_pose is not None :
-        for i in range(7):  
-            pose['panda_joint'+str(i+1)] = joint_pose[i]
+        for i, joint_pose in enumerate(joint_pose):
+            pose['panda_joint'+str(i+1)] = joint_pose
 
-    # ros.loginfo(pose)
 
     topic = ros.resolve_name('franka_state_controller/joint_states')
     # ros.loginfo("move_to_start: Waiting for message on topic '" + topic + "'")
@@ -49,7 +52,7 @@ def go_to_joint_state(joint_pose):
     point.time_from_start = ros.Duration.from_sec(
         # Use either the time to move the furthest joint with 'max_dq' or 500ms,
         # whatever is greater
-        max(max_movement / 0.2, 0.5)
+        max(max_movement / 0.5 , 0.5)
     )
     goal = FollowJointTrajectoryGoal()
 
@@ -60,36 +63,39 @@ def go_to_joint_state(joint_pose):
     goal.goal_time_tolerance = ros.Duration.from_sec(0.5)
 
     # ros.loginfo('Sending trajectory Goal to move into initial config')
-    client.send_goal_and_wait(goal)
+    if end_path:
+        client.send_goal_and_wait(goal)
+    
+        result = client.get_result()
+        if result.error_code != FollowJointTrajectoryResult.SUCCESSFUL:
+            ros.logerr('move_to_start: Movement was not successful: ' + {
+                FollowJointTrajectoryResult.INVALID_GOAL:
+                """
+                The joint pose you want to move to is invalid (e.g. unreachable, singularity...).
+                Is the 'joint_pose' reachable?
+                """,
 
-    result = client.get_result()
-    if result.error_code != FollowJointTrajectoryResult.SUCCESSFUL:
-        ros.logerr('move_to_start: Movement was not successful: ' + {
-            FollowJointTrajectoryResult.INVALID_GOAL:
-            """
-            The joint pose you want to move to is invalid (e.g. unreachable, singularity...).
-            Is the 'joint_pose' reachable?
-            """,
+                FollowJointTrajectoryResult.INVALID_JOINTS:
+                """
+                The joint pose you specified is for different joints than the joint trajectory controller
+                is claiming. Does you 'joint_pose' include all 7 joints of the robot?
+                """,
 
-            FollowJointTrajectoryResult.INVALID_JOINTS:
-            """
-            The joint pose you specified is for different joints than the joint trajectory controller
-            is claiming. Does you 'joint_pose' include all 7 joints of the robot?
-            """,
+                FollowJointTrajectoryResult.PATH_TOLERANCE_VIOLATED:
+                """
+                During the motion the robot deviated from the planned path too much. Is something blocking
+                the robot?
+                """,
 
-            FollowJointTrajectoryResult.PATH_TOLERANCE_VIOLATED:
-            """
-            During the motion the robot deviated from the planned path too much. Is something blocking
-            the robot?
-            """,
+                FollowJointTrajectoryResult.GOAL_TOLERANCE_VIOLATED:
+                """
+                After the motion the robot deviated from the desired goal pose too much. Probably the robot
+                didn't reach the joint_pose properly
+                """,
+            }[result.error_code])
 
-            FollowJointTrajectoryResult.GOAL_TOLERANCE_VIOLATED:
-            """
-            After the motion the robot deviated from the desired goal pose too much. Probably the robot
-            didn't reach the joint_pose properly
-            """,
-        }[result.error_code])
-
-    else:
-        # ros.loginfo('move_to_start: Successfully moved into pose')
-        pass
+        else:
+            # ros.loginfo('move_to_start: Successfully moved into pose')
+            pass
+    else :
+        client.send_goal(goal)
